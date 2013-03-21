b7dc109f8c940012bae9dc13da48ed84b805de31
Jack LaBarba <brakit@gmail.com>
2013-03-21 14:37:47 -0700

    Adds basic tests

--- rhyno/tests/api_test.py
+++ rhyno/tests/api_test.py
@@ -7,6 +7,8 @@ from ..api import Rhyno
 API_HOST = 'https://webprod.plosjournals.org/api'
 SHELL_HOST = 'iad-webprod-devstack01.int.plos.org'
 TEST_PACKAGE_FILENAME = 'pone.0057000.zip'
+TEST_PACKAGE_DOI = 'info:doi/10.1371/journal.pone.0057000'
+DOI_PREFIX = "info:doi/10.1371/journal."
 
 class TestRhinoAPI(unittest.TestCase):
     def setUp(self):     
@@ -14,16 +16,38 @@ class TestRhinoAPI(unittest.TestCase):
     
     def test_ingestible_get(self):
         ret = self.r.ingestible(verbose=True)
-        print("INGESTIBLE DOIS: %s" % ret)
 
     def test_ingest_zip(self):
-        with self.assertRaises(Rhyno.ArticleAlreadyExists):
+        with self.assertRaises(Rhyno.Base405Error):
             self.r.ingest_zip(TEST_PACKAGE_FILENAME, verbose=True)
-            
-        self.r.ingest_zip(TEST_PACKAGE_FILENAME, force_reingest=True, verbose=True)
-        
+        #self.r.ingest_zip(TEST_PACKAGE_FILENAME, force_reingest=True, verbose=True)
 
-        
+    def test_get_article(self):
+        self.r.get_metadata(TEST_PACKAGE_DOI, verbose=True)
+
+    def test_get_article_state(self):
+        self.r._get_state(TEST_PACKAGE_DOI, verbose=True)
+
+    def test_publish(self):
+        self.r.publish(TEST_PACKAGE_DOI, verbose=True)
+        self.assertTrue(self.r.is_published(TEST_PACKAGE_DOI))
+
+    def test_unpublish(self):
+        self.r.unpublish(TEST_PACKAGE_DOI, verbose=True)
+        self.assertFalse(self.r.is_published(TEST_PACKAGE_DOI))
+
+    def test_pmc_syndication_state(self):
+        self.r.get_pmc_syndication_state(TEST_PACKAGE_DOI, verbose=True)
+
+    def test_crossref_syndication_state(self):
+        self.r.get_crossref_syndication_state(TEST_PACKAGE_DOI, verbose=True)
+
+    def test_syndicate_pmc(self):
+        self.r.syndicate_pmc(TEST_PACKAGE_DOI, verbose=True)
+
+    def test_syndicate_crossref(self):
+        self.r.syndicate_crossref(TEST_PACKAGE_DOI, verbose=True)
+    
         
 if __name__ == '__main__':
     unittest.main()
